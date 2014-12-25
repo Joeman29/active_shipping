f8db43e6bb0d9f05815ab1435b1797c54ddb8a3f
Yosef Gitlin <yosef29@gmail.com>
2014-12-25 02:46:53 +0200

    got success for hold at location request

--- test/unit/carriers/fedex_shipment_test.rb
+++ test/unit/carriers/fedex_shipment_test.rb
@@ -11,18 +11,18 @@ class FedExTest < MiniTest::Unit::TestCase
     @tracking_response = xml_fixture('fedex/tracking_response')
   end
 
-  def test_build_shipment_request
-    options = {
-        shipping_options: {
-            service_type: 'FEDEX_GROUND'
-        },
-        # payment_options:{
-        #
-        # }
-        test: true
-    }
-    puts @carrier.create_shipment(@locations[:new_york_with_name], @locations[:annapolis], [@packages[:wii]], options)
-  end
+  # def test_build_shipment_request
+  #   options = {
+  #       shipping_options: {
+  #           service_type: 'FEDEX_GROUND'
+  #       },
+  #       # payment_options:{
+  #       #
+  #       # }
+  #       test: true
+  #   }
+  #   puts @carrier.create_shipment(@locations[:new_york_with_name], @locations[:annapolis], [@packages[:wii]], options)
+  # end
 
   def test_build_shipment_request_with_hold_at_location
     options = {
